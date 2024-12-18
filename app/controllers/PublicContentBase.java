3ed3b5aefc8ebdaf4d2851334137f5f755536898
Javi Espinosa <javier.espinosagar@gmail.com>
2024-12-18 10:27:34 +0100

    Fix length

--- app/controllers/PublicContentBase.java
+++ app/controllers/PublicContentBase.java
@@ -7,28 +7,19 @@ import play.mvc.Controller;
 
 public class PublicContentBase extends Controller {
 
-    private static final int MIN_PASSWORD_LENGTH = 10;
-
+    
     public static void register(){
         render();
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        if (isPasswordValid(password)) {
-            User u = new User(username, HashUtils.getMd5(password), type, -1);
-            u.save();
-            registerComplete();
-        }else {
-            flash.error("Password should be at least 10 characters.");
-            register();
-        }
+        User u = new User(username, HashUtils.getMd5(password), type, -1);
+        u.save();
+        registerComplete();
     }
 
     public static void registerComplete(){
         render();
     }
 
-    private static boolean isPasswordValid(String password) {
-        return password.length() >= 10;
-    }
 }
